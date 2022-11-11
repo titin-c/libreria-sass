826a71ee9db7bd6ed7b9e5582737735c5514c77a
Your NameTitín <titin.colomer.romero@gmail.com>
2022-11-11 15:36:04 +0100

    first commit

---
+++ js/forms/inputFile.js
@@ -0,0 +1,48 @@
+var tempDiv = document.createElement('div');
+tempDiv.className = "form-upload";
+tempDiv.setAttribute("id", "formUpload");
+document.getElementById("inputTypeFile").insertAdjacentElement('afterend', tempDiv);
+// Create element:
+const formUploadText = document.createElement("div");
+formUploadText.className = "form-upload-text";
+formUploadText.setAttribute("id", "formUploadText");
+formUploadText.innerHTML = "Sube o arrastra archivos";
+
+// Append to another element:
+document.getElementById("formUpload").appendChild(formUploadText);
+function uploadFiles() {
+    var x = document.getElementById("inputTypeFile");
+    var txt = "";
+
+
+
+
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Seleccione o arrastre archivos.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<div><strong>" + (i + 1) + ". file</strong><br>";
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "<span>Nombre:</span> " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "<span>Tamaño:</span> " + file.size + " bytes </div>";
+                }
+            }
+        }
+    }
+    else {
+        if (x.value == "") {
+            txt += "Seleccione o arrastre archivos.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+        }
+    }
+
+
+
+    document.getElementById("formUploadText").innerHTML = txt;
+}
